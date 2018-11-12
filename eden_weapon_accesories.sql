1f31efe390dbf573acadc1beebfb09273c7696c7
Delex <20481092+Delex21@users.noreply.github.com>
2018-11-12 09:27:43 +0100

    Update eden_weapon_accesories.sql

--- eden_weapon_accesories.sql
+++ eden_weapon_accesories.sql
@@ -1,4 +1,4 @@
 INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (2010, 'yusuf', 'Yusuf Deluxe Skin', -1, 0, 1);
 INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (209, 'grip', 'Grip', -1, 0, 1);
 INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (208, 'flashlight', 'Light', -1, 0, 1);
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (207, 'silencieux', 'Silencer', -1, 0, 1);
+INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (207, 'silencer', 'Silencer', -1, 0, 1);
