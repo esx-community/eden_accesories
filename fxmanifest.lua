a02e947d950996ca1d7da074c083a92008e9accd
rex2630 <rex2630@gmail.com>
2020-01-17 21:54:26 +0100

    Replace __resource.lua with fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,16 +1,15 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'adamant'
+
+game 'gta5'
 
 description 'Eden Weapon Accesories'
 
 server_scripts {
-
 	'config.lua',
 	'server/main.lua',
-	
 }
 
 client_scripts {
-
 	'config.lua',
 	'client/main.lua'
 }
