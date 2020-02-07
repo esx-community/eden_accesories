bb05b5e358fdb4228c90c1146467c9caf894fd13
rex2630 <rex2630@gmail.com>
2020-02-07 16:11:35 +0100

    Minor cleanup

--- client/main.lua
+++ client/main.lua
@@ -1,4 +1,4 @@
-ESX          = nil
+ESX = nil
 local IsDead = false
 
 Citizen.CreateThread(function()
@@ -13,21 +13,23 @@ AddEventHandler('esx:playerLoaded', function(xPlayer)
   PlayerData = xPlayer 
 end)
 
-				local used = 0
+local used = 0
 
 RegisterNetEvent('eden_accesories:silencieux')
 AddEventHandler('eden_accesories:silencieux', function(duration)
-				local inventory = ESX.GetPlayerData().inventory
-				local silencieux = 0
-					for i=1, #inventory, 1 do
-					  if inventory[i].name == 'silencieux' then
-						silencieux = inventory[i].count
-					  end
-					end
-    local ped = PlayerPedId()
-    local currentWeaponHash = GetSelectedPedWeapon(ped)
-		if used < silencieux then
+	local inventory = ESX.GetPlayerData().inventory
+	local silencieux = 0
 
+		for i=1, #inventory, 1 do
+		  if inventory[i].name == 'silencieux' then
+			silencieux = inventory[i].count
+		  end
+		end
+
+local ped = PlayerPedId()
+local currentWeaponHash = GetSelectedPedWeapon(ped)
+
+		if used < silencieux then
 			if currentWeaponHash == GetHashKey("WEAPON_PISTOL") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_PISTOL"), GetHashKey("component_at_pi_supp_02"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'un silencieux. Il faudra le rééquiper a chaques retours en ville.")) 
@@ -121,24 +123,22 @@ AddEventHandler('eden_accesories:silencieux', function(duration)
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_PUMPSHOTGUN"), GetHashKey("COMPONENT_AT_SR_SUPP"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'un silencieux. Il faudra le rééquiper a chaques retours en ville.")) 
 	used = used + 1
-		  		
+
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_MARKSMANRIFLE") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_MARKSMANRIFLE"), GetHashKey("COMPONENT_AT_AR_SUPP"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'un silencieux. Il faudra le rééquiper a chaques retours en ville.")) 
 	used = used + 1
-		  		
+
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_SNIPERRIFLE") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_SNIPERRIFLE"), GetHashKey("COMPONENT_AT_AR_SUPP_02"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'un silencieux. Il faudra le rééquiper a chaques retours en ville.")) 
 	used = used + 1
-		  		
+
 		  	else 
-		  		  ESX.ShowNotification(("Vous n'avez pas d'arme en main ou votre arme ne peux pas supporter de silencieux."))
-		  		
+		  		  ESX.ShowNotification(("Vous n'avez pas d'arme en main ou votre arme ne peux pas supporter de silencieux."))	
 			end
 			else
 					  		 ESX.ShowNotification(("Vous avez utiliser tout vos silencieux.")) 
-
 		end
 end)
 				local used2 = 0
@@ -152,58 +152,55 @@ AddEventHandler('eden_accesories:flashlight', function(duration)
 						flashlight = inventory[i].count
 					  end
 					end
-    local ped = PlayerPedId()
-    local currentWeaponHash = GetSelectedPedWeapon(ped)
-		if used2 < flashlight then
-						print('used2')
 
+local ped = PlayerPedId()
+local currentWeaponHash = GetSelectedPedWeapon(ped)
+
+		if used2 < flashlight then
 			if currentWeaponHash == GetHashKey("WEAPON_PISTOL") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_PISTOL"), GetHashKey("COMPONENT_AT_PI_FLSH"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'un lampe. Il faudra le rééquiper a chaques retours en ville.")) 
 		  		 	used2 = used2 + 1
+
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_PISTOL50") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_PISTOL50"), GetHashKey("COMPONENT_AT_PI_FLSH"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'un lampe. Il faudra le rééquiper a chaques retours en ville.")) 
 	used2 = used2 + 1
-		  		
 
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_COMBATPISTOL") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_COMBATPISTOL"), GetHashKey("COMPONENT_AT_PI_FLSH"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'un lampe. Il faudra le rééquiper a chaques retours en ville.")) 
 	used2 = used2 + 1
-				
+
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_APPISTOL") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_APPISTOL"), GetHashKey("COMPONENT_AT_PI_FLSH"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'un lampe. Il faudra le rééquiper a chaques retours en ville.")) 
 	used2 = used2 + 1
-		  		 
+
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_HEAVYPISTOL") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_HEAVYPISTOL"), GetHashKey("COMPONENT_AT_PI_FLSH"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'un lampe. Il faudra le rééquiper a chaques retours en ville.")) 
 	used2 = used2 + 1
-		  		
+
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_SMG") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_SMG"), GetHashKey("COMPONENT_AT_AR_FLSH"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'un lampe. Il faudra le rééquiper a chaques retours en ville.")) 
 		  		 	used2 = used2 + 1
 
-
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_MICROSMG") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_MICROSMG"), GetHashKey("COMPONENT_AT_PI_FLSH"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'un lampe. Il faudra le rééquiper a chaques retours en ville.")) 
 	used2 = used2 + 1
-				
 
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_ASSAULTSMG") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_ASSAULTSMG"), GetHashKey("COMPONENT_AT_AR_FLSH"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'un lampe. Il faudra le rééquiper a chaques retours en ville.")) 
 	used2 = used2 + 1
-				 
+ 
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_COMBATPDW") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_COMBATPDW"), GetHashKey("COMPONENT_AT_AR_FLSH"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'un lampe. Il faudra le rééquiper a chaques retours en ville.")) 
-	used2 = used2 + 1
-		  			
+	used2 = used2 + 1	
 
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_ASSAULTRIFLE") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_ASSAULTRIFLE"), GetHashKey("COMPONENT_AT_AR_FLSH"))  
@@ -224,12 +221,12 @@ AddEventHandler('eden_accesories:flashlight', function(duration)
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_SPECIALCARBINE"), GetHashKey("COMPONENT_AT_AR_FLSH"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'un lampe. Il faudra le rééquiper a chaques retours en ville.")) 
 	used2 = used2 + 1
-		  		
+
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_BULLPUPRIFLE") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_BULLPUPRIFLE"), GetHashKey("COMPONENT_AT_AR_FLSH"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'un lampe. Il faudra le rééquiper a chaques retours en ville.")) 
 	used2 = used2 + 1
-		  		
+
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_ASSAULTSHOTGUN") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_ASSAULTSHOTGUN"), GetHashKey("COMPONENT_AT_AR_FLSH"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'un lampe. Il faudra le rééquiper a chaques retours en ville.")) 
@@ -254,105 +251,100 @@ AddEventHandler('eden_accesories:flashlight', function(duration)
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_MARKSMANRIFLE"), GetHashKey("COMPONENT_AT_AR_FLSH"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'un lampe. Il faudra le rééquiper a chaques retours en ville.")) 
 	used2 = used2 + 1
-		  		
+
 		  	else 
 		  		  ESX.ShowNotification(("Vous n'avez pas d'arme en main ou votre arme ne peux pas supporter de lampe."))
-		  		
 			end
 		else
-				  		  ESX.ShowNotification(("Vous avez utiliser toutes vos lampes."))
-
+				  ESX.ShowNotification(("Vous avez utiliser toutes vos lampes."))
 		end
 end)
-				local used3 = 0
+
+local used3 = 0
 
 RegisterNetEvent('eden_accesories:grip')
 AddEventHandler('eden_accesories:grip', function(duration)
-					local inventory = ESX.GetPlayerData().inventory
-				local grip = 0
-					for i=1, #inventory, 1 do
-					  if inventory[i].name == 'grip' then
-						grip = inventory[i].count
-					  end
-					end
-    local ped = PlayerPedId()
-    local currentWeaponHash = GetSelectedPedWeapon(ped)
-		if used3 < grip then
+	local inventory = ESX.GetPlayerData().inventory
+	local grip = 0
+		for i=1, #inventory, 1 do
+		  if inventory[i].name == 'grip' then
+			grip = inventory[i].count
+		  end
+		end
 
-			
+local ped = PlayerPedId()
+local currentWeaponHash = GetSelectedPedWeapon(ped)
+
+		if used3 < grip then
 			if currentWeaponHash == GetHashKey("WEAPON_COMBATPDW") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_COMBATPDW"), GetHashKey("COMPONENT_AT_AR_AFGRIP"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'une poignée. Il faudra le rééquiper a chaques retours en ville.")) 
 		  				used3 = used3 + 1
 
-
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_ASSAULTRIFLE") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_ASSAULTRIFLE"), GetHashKey("COMPONENT_AT_AR_AFGRIP"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'une poignée. Il faudra le rééquiper a chaques retours en ville.")) 
 	used3 = used3 + 1
-		  		
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_CARBINERIFLE") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_CARBINERIFLE"), GetHashKey("COMPONENT_AT_AR_AFGRIP"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'une poignée. Il faudra le rééquiper a chaques retours en ville.")) 
-	used3 = used3 + 1
-		  		
-		  		
+	used3 = used3 + 1	
+
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_SPECIALCARBINE") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_SPECIALCARBINE"), GetHashKey("COMPONENT_AT_AR_AFGRIP"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'une poignée. Il faudra le rééquiper a chaques retours en ville.")) 
 	used3 = used3 + 1
-		  		
+
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_BULLPUPRIFLE") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_BULLPUPRIFLE"), GetHashKey("COMPONENT_AT_AR_AFGRIP"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'une poignée. Il faudra le rééquiper a chaques retours en ville.")) 
 	used3 = used3 + 1
-		  		
+
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_ASSAULTSHOTGUN") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_ASSAULTSHOTGUN"), GetHashKey("COMPONENT_AT_AR_AFGRIP"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'une poignée. Il faudra le rééquiper a chaques retours en ville.")) 
 	used3 = used3 + 1
-		  		
+
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_HEAVYSHOTGUN") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_HEAVYSHOTGUN"), GetHashKey("COMPONENT_AT_AR_AFGRIP"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'une poignée. Il faudra le rééquiper a chaques retours en ville.")) 
 	used3 = used3 + 1
-		  		
+
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_BULLPUPSHOTGUN") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_BULLPUPSHOTGUN"), GetHashKey("COMPONENT_AT_AR_AFGRIP"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'une poignée. Il faudra le rééquiper a chaques retours en ville.")) 
 	used3 = used3 + 1
-		  		 
-		  		
+
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_MARKSMANRIFLE") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_MARKSMANRIFLE"), GetHashKey("COMPONENT_AT_AR_AFGRIP"))  
 		  		 ESX.ShowNotification(("Vous venez de vous équiper d'une poignée. Il faudra le rééquiper a chaques retours en ville.")) 
 	used3 = used3 + 1
-		  		
+
 		  	else 
 		  		  ESX.ShowNotification(("Vous n'avez pas d'arme en main ou votre arme ne peux pas supporter de poignée."))
-		  		
+
 			end
 		else
-				  		  ESX.ShowNotification(("Vous avez utiliser toutes vos poignées."))
+				  ESX.ShowNotification(("Vous avez utiliser toutes vos poignées."))
 		end
 end)
 
-				local used4 = 0
+local used4 = 0
 
 RegisterNetEvent('eden_accesories:yusuf')
 AddEventHandler('eden_accesories:yusuf', function(duration)
-					local inventory = ESX.GetPlayerData().inventory
-				local yusuf = 0
-					for i=1, #inventory, 1 do
-					  if inventory[i].name == 'yusuf' then
-						yusuf = inventory[i].count
-					  end
-					end
-					
-    local ped = PlayerPedId()
-    local currentWeaponHash = GetSelectedPedWeapon(ped)
-		if used4 < yusuf then
+	local inventory = ESX.GetPlayerData().inventory
+	local yusuf = 0
+		for i=1, #inventory, 1 do
+		  if inventory[i].name == 'yusuf' then
+			yusuf = inventory[i].count
+		  end
+		end
+		
+local ped = PlayerPedId()
+local currentWeaponHash = GetSelectedPedWeapon(ped)
 
+		if used4 < yusuf then
 			if currentWeaponHash == GetHashKey("WEAPON_PISTOL") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_PISTOL"), GetHashKey("COMPONENT_PISTOL_VARMOD_LUXE"))  
 		  		 ESX.ShowNotification(("Vous venez d'équiper votre arme skin. Il faudra le rééquiper a chaques retours en ville.")) 
@@ -362,13 +354,12 @@ AddEventHandler('eden_accesories:yusuf', function(duration)
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_PISTOL50"), GetHashKey("COMPONENT_PISTOL50_VARMOD_LUXE"))  
 		  		 ESX.ShowNotification(("Vous venez d'équiper votre arme skin. Il faudra le rééquiper a chaques retours en ville.")) 
 	used4 = used4 + 1
-		  		
-				
+
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_APPISTOL") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_APPISTOL"), GetHashKey("COMPONENT_APPISTOL_VARMOD_LUXE"))  
 		  		 ESX.ShowNotification(("Vous venez d'équiper votre arme skin. Il faudra le rééquiper a chaques retours en ville.")) 
 	used4 = used4 + 1
-		  		 
+
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_HEAVYPISTOL") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_HEAVYPISTOL"), GetHashKey("COMPONENT_HEAVYPISTOL_VARMOD_LUXE"))  
 		  		 ESX.ShowNotification(("Vous venez d'équiper votre arme skin. Il faudra le rééquiper a chaques retours en ville.")) 
@@ -378,14 +369,11 @@ AddEventHandler('eden_accesories:yusuf', function(duration)
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_SMG"), GetHashKey("COMPONENT_SMG_VARMOD_LUXE"))  
 		  		 ESX.ShowNotification(("Vous venez d'équiper votre arme skin. Il faudra le rééquiper a chaques retours en ville.")) 
 	used4 = used4 + 1
-		  		 
 
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_MICROSMG") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_MICROSMG"), GetHashKey("COMPONENT_MICROSMG_VARMOD_LUXE"))  
 		  		 ESX.ShowNotification(("Vous venez d'équiper votre arme skin. Il faudra le rééquiper a chaques retours en ville.")) 
 	used4 = used4 + 1
-				
-
 
 		  	elseif currentWeaponHash == GetHashKey("WEAPON_ASSAULTRIFLE") then
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_ASSAULTRIFLE"), GetHashKey("COMPONENT_ASSAULTRIFLE_VARMOD_LUXE"))  
@@ -401,19 +389,14 @@ AddEventHandler('eden_accesories:yusuf', function(duration)
 		  		 GiveWeaponComponentToPed(GetPlayerPed(-1), GetHashKey("WEAPON_ADVANCEDRIFLE"), GetHashKey("COMPONENT_ADVANCEDRIFLE_VARMOD_LUXE"))  
 		  		 ESX.ShowNotification(("Vous venez d'équiper votre arme skin. Il faudra le rééquiper a chaques retours en ville.")) 
 	used4 = used4 + 1
-		  		
-		  	
+
 		  	else 
 		  		  ESX.ShowNotification(("Vous n'avez pas d'arme en main ou votre arme ne peux pas supporter de look de luxe."))
-		  		
 			end
 		else
-				  		  ESX.ShowNotification(("Vous avez utiliser tout vos skins de luxe."))
-
+				  ESX.ShowNotification(("Vous avez utiliser tout vos skins de luxe."))
 		end
 end)
-
-
 
 AddEventHandler('playerSpawned', function()
   used = 0
@@ -421,15 +404,3 @@ AddEventHandler('playerSpawned', function()
   used3 = 0
   used4 = 0
 end)
-
-
-
-
-
-
-
-
-
-
-
-
