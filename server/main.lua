6e95c74cf11c94fb22f25e5f44f976344374bf15
rex2630 <rex2630@gmail.com>
2020-02-07 16:13:45 +0100

    Minor cleanup

--- server/main.lua
+++ server/main.lua
@@ -2,9 +2,9 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-
 ESX.RegisterUsableItem('silencieux', function(source)
-    local xPlayer = ESX.GetPlayerFromId(source)	
+    local xPlayer = ESX.GetPlayerFromId(source)
+
     TriggerClientEvent('eden_accesories:silencieux', source)
 
 end)
@@ -13,21 +13,16 @@ ESX.RegisterUsableItem('flashlight', function(source)
     local xPlayer = ESX.GetPlayerFromId(source)	
 	
     TriggerClientEvent('eden_accesories:flashlight', source)
-	
 end)
 
 ESX.RegisterUsableItem('grip', function(source)
     local xPlayer = ESX.GetPlayerFromId(source)
 		
     TriggerClientEvent('eden_accesories:grip', source)
-	
 end)
-
 
 ESX.RegisterUsableItem('yusuf', function(source)
     local xPlayer = ESX.GetPlayerFromId(source)
 
     TriggerClientEvent('eden_accesories:yusuf', source)
-
 end)
-
