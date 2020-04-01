e5a46976a58ffe1768e79cb46c8f96b76a9ac476
KoolAiddTV <51595577+KoolAiddTV@users.noreply.github.com>
2020-04-01 00:14:33 -0400

    Update fr_eden_weapon_accesories.sql

--- fr_eden_weapon_accesories.sql
+++ fr_eden_weapon_accesories.sql
@@ -1,4 +1,4 @@
-INSERT INTO `items` (`id`, `name`, `label`, `weight`, `rare`, `can_remove`) VALUES (2010, 'yusuf', 'Skin de luxe', 1, 0, 1);
-INSERT INTO `items` (`id`, `name`, `label`, `weight`, `rare`, `can_remove`) VALUES (209, 'grip', 'Poignée', 1, 0, 1);
-INSERT INTO `items` (`id`, `name`, `label`, `weight`, `rare`, `can_remove`) VALUES (208, 'flashlight', 'Lampe', 1, 0, 1);
-INSERT INTO `items` (`id`, `name`, `label`, `weight`, `rare`, `can_remove`) VALUES (207, 'silencieux', 'Silencieux', 1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('yusuf', 'Skin de luxe', 1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('grip', 'Poignée', 1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('flashlight', 'Lampe', 1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('silencieux', 'Silencieux', 1, 0, 1);
