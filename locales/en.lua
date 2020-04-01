318943e5552f139e4470fc554f061fa09258eb81
KoolAiddTV <51595577+KoolAiddTV@users.noreply.github.com>
2020-04-01 00:12:08 -0400

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,15 @@
+Locales['en'] = {
+    ['silencieux_equipped']     = 'You have just equipped yourself with a silencer. It will have to be re-equipped with each return to town.',
+    ['silencieux_nosupport']    = 'You do not have a weapon in your hand or your weapon cannot support a silencer.',
+    ['silencieux_none']         = 'No silencers?',
+    ['flashlight_equipped']     = 'You have just equipped yourself with a flashlight. It will have to be re-equipped with each return to town.',
+    ['flashlight_nosupport']    = 'You have no weapon in hand or your weapon cannot support a flashlight.',
+    ['flashlight_none']         = 'No flashlights?',
+    ['grip_equipped']           = 'You have just equipped yourself with a grip. It will have to be re-equipped with each return to town.',
+    ['grip_nosupport']          = 'You do not have a weapon in your hand or your weapon cannot support a handle.',
+    ['grip_none']               = 'No grips?',
+    ['yusuf_equipped']          = 'You have just fitted your weapon. It will have to be re-equipped with each return to town.',
+    ['yusuf_nosupport']         = 'You do not have a weapon in your hand or your weapon cannot bear a luxury look.',
+    ['yusuf_none']              = 'No weapon skins?',
+}
+
