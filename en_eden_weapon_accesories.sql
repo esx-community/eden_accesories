ef1cf016a3354b418c11e7f712f709aedbba172a
KoolAiddTV <51595577+KoolAiddTV@users.noreply.github.com>
2020-04-01 00:16:27 -0400

    Create en_eden_weapon_accesories.sql

---
+++ en_eden_weapon_accesories.sql
@@ -0,0 +1,4 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('yusuf', 'Weapon Skin', 1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('grip', 'Weapon Grip', 1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('flashlight', 'Weapon Flashlight', 1, 0, 1);
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('silencieux', 'Weapon Silencer', 1, 0, 1);
